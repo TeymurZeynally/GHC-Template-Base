77e1847d4ecadaf81c909c1c94a996632b204588
TeymurZeynally <z.teymur.e@mail.ru>
2022-01-02 20:23:45 +0300

    edit entrypoint2

--- .build/entrypoint.sh
+++ .build/entrypoint.sh
@@ -1,7 +1,5 @@
-sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-runtime-6.0
+sudo apt-get update 
+sudo apt-get install -y dotnet-runtime-6.0
 
 
 dotnet --list-sdks
